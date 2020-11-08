bc5543d14959ef68eea9410e8414b3c786d149a2
Limlin-Qs <limlin95@126.com>
2020-11-08 22:03:48 +0800

    project initialized

---
+++ src/main/java/com/limlin/centurylib/CenturylibApplication.java
@@ -0,0 +1,13 @@
+package com.limlin.centurylib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenturylibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CenturylibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/limlin/centurylib/CenturylibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limlin.centurylib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenturylibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
